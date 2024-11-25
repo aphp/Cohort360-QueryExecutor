9ce2a5fd6ca85f9015d321ca5a04e0cc3f7944bc
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2024-11-25 17:14:40 +0100

    feat: add sampled cohort creation (#9)

--- src/test/scala/fr/aphp/id/eds/requester/CreateQueryTest.scala
+++ src/test/scala/fr/aphp/id/eds/requester/CreateQueryTest.scala
@@ -3,11 +3,12 @@ package fr.aphp.id.eds.requester
 import fr.aphp.id.eds.requester.cohort.CohortCreation
 import fr.aphp.id.eds.requester.jobs.{JobEnv, JobsConfig, SparkJobParameter}
 import fr.aphp.id.eds.requester.query.engine.QueryBuilder
+import fr.aphp.id.eds.requester.query.model.SourcePopulation
 import fr.aphp.id.eds.requester.query.resolver.{ResourceResolver, ResourceResolvers}
 import fr.aphp.id.eds.requester.tools.JobUtilsService
-import org.apache.spark.sql.SparkSession
-import org.mockito.ArgumentMatchersSugar
-import org.mockito.MockitoSugar.{mock, when}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.mockito.{ArgumentCaptor, ArgumentMatchersSugar}
+import org.mockito.MockitoSugar.{mock, spy, verify, when}
 import org.scalatest.funsuite.AnyFunSuiteLike
 
 import java.nio.file.Paths
@@ -60,7 +61,7 @@ class CreateQueryTest extends AnyFunSuiteLike {
       .getOrCreate()
 
     val queryBuilderMock = mock[QueryBuilder]
-    val omopTools = mock[CohortCreation]
+    val omopTools = spy(mock[CohortCreation])
     val resourceResolver = ResourceResolver.get(ResourceResolvers.solr)
     class JobUtilsMock extends JobUtilsService {
       override def getRandomIdNotInTabooList(allTabooId: List[Short], negative: Boolean): Short = 99
@@ -128,7 +129,7 @@ class CreateQueryTest extends AnyFunSuiteLike {
     }
     assert(error.getMessage == "Non-patient resource filter request should be a basic resource")
 
-    val expected = getClass.getResource(s"/testCases/simple/expected.csv")
+    val expected = getClass.getResource(s"/testCases/stageRatioDetails/expected.csv")
     val expectedResult = sparkSession.read
       .format("csv")
       .option("delimiter", ";")
@@ -146,6 +147,14 @@ class CreateQueryTest extends AnyFunSuiteLike {
         ArgumentMatchersSugar.*
       )
     ).thenReturn(expectedResult)
+    when(omopTools.createCohort(
+      ArgumentMatchersSugar.eqTo("testCohortSimple"),
+      ArgumentMatchersSugar.any[Option[String]],
+      ArgumentMatchersSugar.any[String],
+      ArgumentMatchersSugar.any[String],
+      ArgumentMatchersSugar.any[String],
+      ArgumentMatchersSugar.any[Long]
+    )).thenReturn(0)
     val request =
       """
       {"cohortUuid":"ecd89963-ac90-470d-a397-c846882615a6","sourcePopulation":{"caresiteCohortList":[31558]},"_type":"request","request":{"_type":"andGroup","_id":0,"isInclusive":true,"criteria":[{"_type":"basicResource","_id":1,"isInclusive":true,"resourceType":"patientAphp","filterSolr":"fq=gender:f&fq=deceased:false&fq=active:true","filterFhir":"active=true&gender=f&deceased=false&age-day=ge0&age-day=le130"}],"temporalConstraints":[]}}"
@@ -154,15 +163,50 @@ class CreateQueryTest extends AnyFunSuiteLike {
       sparkSession,
       JobEnv("someid", AppConfig.get),
       SparkJobParameter(
-        "testCohort",
+        "testCohortSimple",
         None,
         request,
         "someOwnerId"
       )
     )
     assert(res.status == "FINISHED")
-    assert(res.data("group.count") == "2")
+    assert(res.data("group.count") == "6")
     assert(res.data("group.id") == "0")
+
+    when(omopTools.createCohort(
+      ArgumentMatchersSugar.eqTo("testCohortSampling"),
+      ArgumentMatchersSugar.any[Option[String]],
+      ArgumentMatchersSugar.any[String],
+      ArgumentMatchersSugar.any[String],
+      ArgumentMatchersSugar.any[String],
+      ArgumentMatchersSugar.any[Long]
+    )).thenReturn(1L)
+    val sampled = createJob.runJob(
+      sparkSession,
+      JobEnv("someid", AppConfig.get),
+      SparkJobParameter(
+        "testCohortSampling",
+        None,
+        request,
+        "someOwnerId",
+        modeOptions = Map("sampling" -> "0.33")
+      )
+    )
+    val omopToolsCaptor = ArgumentCaptor.forClass(classOf[org.apache.spark.sql.DataFrame])
+    verify(omopTools).updateCohort(
+      ArgumentMatchersSugar.eqTo(1L),
+      omopToolsCaptor.capture(),
+      ArgumentMatchersSugar.any[SourcePopulation],
+      ArgumentMatchersSugar.any[Long],
+      ArgumentMatchersSugar.any[Boolean],
+      ArgumentMatchersSugar.any[String]
+    )
+    val capturedDataFrame: DataFrame = omopToolsCaptor.getValue
+    assert(capturedDataFrame.columns.contains("subject_id"))
+    assert(capturedDataFrame.count() >= 1 && capturedDataFrame.count() <= 2)
+    assert(sampled.status == "FINISHED")
+    assert(sampled.data("group.count").toInt >= 1 && sampled.data("group.count").toInt <= 2)
+    assert(sampled.data("group.id") == "1")
   }
 
 }
