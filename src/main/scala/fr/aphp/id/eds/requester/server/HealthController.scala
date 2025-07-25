3092e94f36d4392577f418303204475dd06ea5eb
Paul Bui-Quang <paul.bui-quang@aphp.fr>
2025-07-25 10:49:58 +0200

    feat(health): return 503 error if spark context is stopped

--- src/main/scala/fr/aphp/id/eds/requester/server/HealthController.scala
+++ src/main/scala/fr/aphp/id/eds/requester/server/HealthController.scala
@@ -1,10 +1,18 @@
 package fr.aphp.id.eds.requester.server
 
+import fr.aphp.id.eds.requester.jobs.SparkConfig
+import org.apache.spark.sql.SparkSession
 import org.scalatra.ScalatraServlet
 
-class HealthController extends ScalatraServlet {
+class HealthController(val sparkSession: SparkSession = SparkConfig.sparkSession)
+    extends ScalatraServlet {
 
   get("/") {
-    "OK"
+    if (sparkSession != null && !sparkSession.sparkContext.isStopped) {
+      "OK"
+    } else {
+      halt(503, "Service Unavailable: Spark context is stopped")
+    }
   }
+
 }
